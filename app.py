c112ff000a6e76f6464f197a72908dfa5e5cecf4
José Neto <73612432+jose-raposo@users.noreply.github.com>
2022-06-23 18:15:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_restful import reqparse, abort, Api, Resource
 import joblib
 import numpy as np
@@ -11,8 +11,8 @@ api = Api(app)
 # loading model
 model = joblib.load('model.pkl')
 
+# argument parsing
 try:
-    # argument parsing
     parser = reqparse.RequestParser()
     parser.add_argument('idade', type=float)
     parser.add_argument('esv', type = float, action='append')
@@ -28,36 +28,50 @@ except:
 
 class PredictProbability(Resource):
     def get(self):
-
         # use parser and find the user's query
         args = parser.parse_args()
-        user_query1 = args['idade']
-        user_query2 = args['esv']
-        user_query3 = args['essv']
-        user_query4 = args['imve']
-        user_query5 = args['vae']
-        user_query6 = args['u']
-        user_query7 = args['creat']
-        user_query8 = args['k']
-        user_query9 = args['ct']
 
+        #user_query1 = args['idade']
+        #user_query2 = args['esv']
+        #user_query3 = args['essv']
+        #user_query4 = args['imve']
+        #user_query5 = args['vae']
+        #user_query6 = args['u']
+        #user_query7 = args['creat']
+        #user_query8 = args['k']
+        #user_query9 = args['ct']
+        try:
+            idade = float(request.args.get('idade'))
+            esv = float(request.args.get('esv'))
+            essv = float(request.args.get('essv'))
+            imve = float(request.args.get('imve'))
+            vae = float(request.args.get('vae'))
+            u = float(request.args.get('u'))
+            creat = float(request.args.get('creat'))
+            k = float(request.args.get('k'))
+            ct = float(request.args.get('ct'))
+        except:
+            pass
 
         # converting to np.array
-        user_query1 = np.asarray(user_query1)
-        user_query2 = np.asarray(user_query2)
-        user_query3 = np.asarray(user_query3)
-        user_query4 = np.asarray(user_query4)
-        user_query5 = np.asarray(user_query5)
-        user_query6 = np.asarray(user_query6)
-        user_query7 = np.asarray(user_query7)
-        user_query8 = np.asarray(user_query8)
-        user_query9 = np.asarray(user_query9)
+        #user_query1 = np.asarray(user_query1)
+        #user_query2 = np.asarray(user_query2)
+        #user_query3 = np.asarray(user_query3)
+        #user_query4 = np.asarray(user_query4)
+        #user_query5 = np.asarray(user_query5)
+        #user_query6 = np.asarray(user_query6)
+        #user_query7 = np.asarray(user_query7)
+        #user_query8 = np.asarray(user_query8)
+        #user_query9 = np.asarray(user_query9)
 
         # run predictions on the array of features
         try:
-            pred_proba = model.predict_proba(np.array([user_query1, user_query2,
-                user_query3, user_query4, user_query5, user_query6,
-                user_query7, user_query8, user_query9]).reshape(-1, 9))[:,1]
+            #pred_proba = model.predict_proba(np.array([user_query1, user_query2,
+             #   user_query3, user_query4, user_query5, user_query6,
+              #  user_query7, user_query8, user_query9]).reshape(-1, 9))[:,1]
+            pred_proba = model.predict_proba(np.array([idade, esv,
+                essv, imve, vae, u,
+                creat, k, ct]).reshape(-1, 9))[:,1]
 
 
             # round the predict proba value and set to new variable
